0ac297b76e9d7bf7497b0f18524c3cf16e4b507c
Aryan Singhal <aryan.22scse1180055@galgotiasuniversity.edu.in>
2024-12-02 12:08:51 +0530

    Initial Commit

---
+++ Team25_SCM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team25_SCM
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Team25_SCMTests/Team25_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team25_SCMTests.swift
+//  Team25_SCMTests
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import Testing
+@testable import Team25_SCM
+
+struct Team25_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
